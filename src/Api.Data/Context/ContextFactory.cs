3dd7f7b2115d77cae98ff140bf56969b7193c1b1
Romulohenriquesc <romulohsc7@gmail.com>
2021-06-07 18:29:41 -0300

    Update

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,7 +8,7 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Used for create the migrations
-            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=241419ro";
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=YOUR_PASSWORD";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
